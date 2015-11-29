023fc338ba663115e9ef76690fa68f2379b5a34c
Fannin <BFannin@redwoodsgroup.com>
2015-11-29 10:01:19 -0500

    Fix COTOR data

--- data-raw/COTOR.R
+++ data-raw/COTOR.R
@@ -1,4 +1,4 @@
-library(xlsx)
+library(readxl)
 library(reshape2)
 
 strCOTOR2 <- "./data-raw/COTORChallenge2Claims.xls"
@@ -6,11 +6,11 @@ strCOTOR3 <- "./data-raw/COTORChallenge3Claims.xls"
 strCOTOR4 <- "./data-raw/ChallengeRound4data.xls"
 strCOTOR5 <- "./data-raw/ChallengeRound5data.xls"
 
-COTOR2 <- read.xlsx(strCOTOR2, sheetIndex = 1, startRow = 2, endRow = 251)
-COTOR2 <- COTOR2[, 1]
+COTOR2 <- read_excel(strCOTOR2, sheet = 1)
+names(COTOR2) <- "Claim"
 save(COTOR2, file = "./data/COTOR2.rdata", compress = "xz")
 
-COTOR3 <- read.xlsx(strCOTOR3, sheetIndex = 1, startRow = 3, endRow = 72)
+COTOR3 <- read_excel(strCOTOR3, sheet = 1, skip = 1)
 names(COTOR3) <- 1:7
 COTOR3$ClaimNumber <- as.character(row.names(COTOR3))
 COTOR3 <- melt(COTOR3, id.vars = "ClaimNumber", variable.name = "Year"
@@ -18,7 +18,7 @@ COTOR3 <- melt(COTOR3, id.vars = "ClaimNumber", variable.name = "Year"
 
 save(COTOR3, file = "./data/COTOR3.rdata", compress = "xz")
 
-COTOR4 <- read.xlsx(strCOTOR4, sheetName = "Sheet1", startRow = 14, endRow = 514)
+COTOR4 <- read_excel(strCOTOR4, sheet = "Sheet1", skip = 13)
 names(COTOR4) <- c("ClaimNumber", 1:5)
 
 COTOR4 <- melt(COTOR4, id.vars = "ClaimNumber", variable.name = "Year"
@@ -26,6 +26,6 @@ COTOR4 <- melt(COTOR4, id.vars = "ClaimNumber", variable.name = "Year"
 
 save(COTOR4, file = "./data/COTOR4.rdata", compress = "xz")
 
-COTOR5 <- read.xlsx(strCOTOR5, sheetName = "Round5", startRow = 1, endRow = 4850, colIndex = 1:4)
+COTOR5 <- read_excel(strCOTOR5, sheet = "Round5")
 
 save(COTOR5, file = "./data/COTOR5.rdata", compress = "xz")
