9aae8d7e0deba7dfb6abc62e80a8532c0f656772
Brian Fannin <FanninQED@Yahoo.com>
2016-03-01 22:59:45 -0500

    Various housekeeping

--- R/NAIC.R
+++ R/NAIC.R
@@ -41,3 +41,30 @@ NULL
 #' @name comauto
 #' @docType data
 NULL
+
+#' 
+#' Complete triangle of losses for many medmal companies. Data is taken from the CAS.
+#' 
+#' @format Data frame with 400 observations of 14 variables
+#' @source \url{http://www.casact.org/research/reserve_data/ppauto_pos.csv}
+#' @name medmal
+#' @docType data
+NULL
+
+#' 
+#' Complete triangle of losses for many other liability companies. Data is taken from the CAS.
+#' 
+#' @format Data frame with 400 observations of 14 variables
+#' @source \url{http://www.casact.org/research/reserve_data/ppauto_pos.csv}
+#' @name othliab
+#' @docType data
+NULL
+
+#' 
+#' Complete triangle of losses for many product liability companies. Data is taken from the CAS.
+#' 
+#' @format Data frame with 400 observations of 14 variables
+#' @source \url{http://www.casact.org/research/reserve_data/ppauto_pos.csv}
+#' @name prodliab
+#' @docType data
+NULL
---
+++ R/PPA.R
@@ -0,0 +1,29 @@
+#' Accident year
+#' @name PPA_AccidentYear
+#' @docType data
+NULL
+
+#' Loss development
+#' @name PPA_LossDevelopment
+#' @docType data
+NULL
+
+#' Loss trend
+#' @name PPA_LossTrend
+#' @docType data
+NULL
+
+#' Premium trend
+#' @name PPA_PremiumTrend
+#' @docType data
+NULL
+
+#' Rate change
+#' @name PPA_RateChange
+#' @docType data
+NULL
+
+#' ULAE
+#' @name PPA_ULAE
+#' @docType data
+NULL
